c2abb8ac08af905933ee6b25c3f31bf788462372
WJJK <60299597+WJJK@users.noreply.github.com>
2020-10-03 15:26:03 +0200

    Add files via upload

---
+++ bank/accounts/src/main/java/pl/rsfot/accounts/AccountsApplication.java
@@ -0,0 +1,15 @@
+package pl.rsfot.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
